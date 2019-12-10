4fe800a69227e451c515fda298198dad323d0521
shuja.khalid <s.khalid@surgicalsafety.com>
2019-12-10 17:53:31 -0500

    updated logging parameters

--- DEQModel/modules/deq.py
+++ DEQModel/modules/deq.py
@@ -103,7 +103,10 @@ class DummyDEQFunc(Function):
         
         y.backward(torch.zeros_like(dl_df_est), retain_graph=False)
 
-        print('clone_grad: {}'.format(z1ss_temp.grad.clone().detach()))
+        grad_f_x = z1ss_temp.grad.clone().detach()
+        print('clone_grad: {}'.format((grad_f_x).shape))
+        print('||grad||2: {}'.format(torch.norm(grad_f_x)))
+        print('(||grad||2)**2: {}'.format(torch.norm(grad_f_x)**2))
 
         grad_args = [None for _ in range(len(args))]
         return (None, dl_df_est, None, None, *grad_args)
