a0de4b72ba4e6194bb5504b118159f34a78a84d8
shuja.khalid <s.khalid@surgicalsafety.com>
2019-12-11 10:38:38 -0500

    added regularization term in deq! Let the regularization begin\!

--- DEQModel/train_transformer.py
+++ DEQModel/train_transformer.py
@@ -398,7 +398,7 @@ def train():
 
             # Gradient with respect to the equilibrium point
             # is calculated in DummyDEQFunc.backward()
-            reg = 0.6*(torch.norm(g_f_x.float().mean().type_as(loss))**2)
+            reg = 0.4*(torch.norm(g_f_x.float().mean().type_as(loss))**2)
             loss = loss.float().mean().type_as(loss) + reg
 
             #print('loss: {}'.format(loss))
