276d00077e408daa6f74b3cbdd69167829a47365
shuja.khalid <s.khalid@surgicalsafety.com>
2019-12-11 11:16:13 -0500

    Working on solver regularization to decrease qty of iterations

--- DEQModel/run_wt103_deq_transformer.sh
+++ DEQModel/run_wt103_deq_transformer.sh
@@ -21,8 +21,8 @@ if [[ $1 == 'train' ]]; then
         --lr 0.00025 \
         --warmup_step 0 \
         --pretrain_steps 0 \
-        --log_interval 200 \
-        --eval_interval 5000 \
+        --log_interval 1 \
+        --eval_interval 5 \
         --max_step 5 \
         --tgt_len 150 \
         --mem_len 150 \
