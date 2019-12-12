6621635ac57aee877396871d2526f75e71bc7c01
shuja.khalid <s.khalid@surgicalsafety.com>
2019-12-12 17:51:21 -0500

    Working on solver regularization to decrease qty of iterations

--- DEQModel/modules/deq.py
+++ DEQModel/modules/deq.py
@@ -40,7 +40,8 @@ class DEQFunc(Function):
 
         # \nabla calc =================================================
         z1ss_est_temp = z1ss.clone().detach().requires_grad_()
-        func_copy = func.copy()
+        func_copy = func
+        func_copy = copy.deepcopy(func)
 
         with torch.enable_grad():
             y = DEQFunc.f(func_copy, z1ss_est_temp, uss, z0, *args)
