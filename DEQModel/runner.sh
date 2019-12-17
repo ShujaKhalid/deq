64314c7d54bf1a419637a72d36d2083f66056416
shuja.khalid <s.khalid@surgicalsafety.com>
2019-12-17 16:14:26 -0500

    Long Live Neumann

--- DEQModel/runner.sh
+++ DEQModel/runner.sh
@@ -33,13 +33,13 @@
 #srun --gres=gpu:4 -c 24 -l -w guppy34 bash run_wt103_deq_transformer.sh train --name 1.0sgd_0.0fpm --reg_sgd 1.0 --reg_fpm 0.0
 
 # Regularization term to the training (perplexity) objective (txp GPUs for consistency)
-#srun --gres=gpu:4 -c 24 -l -w guppy34 bash run_wt103_deq_transformer.sh train --name 0.0sgd_0.0fpm_brdn --reg_sgd 0.0 --reg_fpm 0.0
+srun --gres=gpu:4 -c 24 -l -w guppy34 bash run_wt103_deq_transformer.sh train --name 0.0sgd_0.0fpm_brdn --reg_sgd 0.0 --reg_fpm 0.0
 #srun --gres=gpu:4 -c 24 -l -w guppy31 bash run_wt103_deq_transformer.sh train --name 0.0sgd_0.0fpm_nmen --reg_sgd 0.0 --reg_fpm 0.0
 
 #srun --gres=gpu:4 -c 24 -l -w guppy31 bash run_wt103_deq_transformer.sh train --name 0.2sgd_0.0fpm --reg_sgd 0.2 --reg_fpm 0.0
 #srun --gres=gpu:4 -c 24 -l -w guppy25 bash run_wt103_deq_transformer.sh train --name 0.4sgd_0.0fpm --reg_sgd 0.4 --reg_fpm 0.0
 #srun --gres=gpu:4 -c 24 -l -w guppy35 bash run_wt103_deq_transformer.sh train --name 0.6sgd_0.0fpm --reg_sgd 0.6 --reg_fpm 0.0
-#srun --gres=gpu:4 -c 24 -l -w guppy34 bash run_wt103_deq_transformer.sh train --name 0.8sgd_0.0fpm --reg_sgd 0.8 --reg_fpm 0.0
+#srun --gres=gpu:4 -c 24 -l -w guppy36 bash run_wt103_deq_transformer.sh train --name 0.8sgd_0.0fpm --reg_sgd 0.8 --reg_fpm 0.0
 #srun --gres=gpu:4 -c 24 -l -w guppy28 bash run_wt103_deq_transformer.sh train --name 1.0sgd_0.0fpm --reg_sgd 1.0 --reg_fpm 0.0
 
 
