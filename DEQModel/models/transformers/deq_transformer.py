789b4d01f5e0faacc448738446f1cef18b6f17bb
shuja.khalid <s.khalid@surgicalsafety.com>
2019-12-18 08:59:09 -0500

    Long Live Neumann

--- DEQModel/models/transformers/deq_transformer.py
+++ DEQModel/models/transformers/deq_transformer.py
@@ -392,6 +392,7 @@ class DEQTransformerLM(nn.Module):
             with torch.enable_grad():
                 F = self.func(z1ss_est_temp, us, z0, pos_emb)
 
+            print(z1ss_est_temp)            
             print(torch.sum(F))            
             g_f_x = torch.autograd.grad(torch.sum(F), z1ss_est_temp, create_graph=True)[0]          
             print(torch.norm(g_f_x,2)**2)  
